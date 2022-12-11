70b3556e366815958e5a881b2ca3cc9534fa8c28
Descendo The Monkei <61548447+hayden-droid@users.noreply.github.com>
2022-12-11 14:16:54 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<h2>ShareFile by kirillsaint</h2>
+<h2>Memeitizer File Share</h2>
 <form method="post" action="core/upload.php" enctype="multipart/form-data">
     <input type="file" name="image">
     <button type="submit">Upload</button>
